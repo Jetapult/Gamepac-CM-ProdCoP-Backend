69eb30d8279cfde34011bad45bfedfb0e1e9381e
Snehal Shyamsukha <snehalshyamsukha@Snehal-Shyamsukha.local>
2023-09-01 13:46:20 +0530

    changed port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const middleware=require('./middlewares/index')
 require('dotenv').config();
 
 const routes =require('./routes/routes');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 // app.use(middleware.decodeToken);
