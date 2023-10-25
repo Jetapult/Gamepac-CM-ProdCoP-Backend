f6a0c76eecdbf28dd1794a2f1067cd39a844e5b2
Snehal Shyamsukha <snehalshyamsukha@Snehal-Shyamsukha.local>
2023-10-25 12:08:53 +0530

    changed prompts

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -505,8 +505,11 @@ const summary= async (req, res) => {
     const openaiResponse = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: "gpt-3.5-turbo-16k",
-        messages: [{"role": "system", "content": "You are a secretary that deeply reads and understands text transcriptions created by whisper, You summarise the text without missing any crucial points and create a summary. You list out the summary in points so it is more consumable for the users."}, {role: "user", content: `${transcription}`}],
+        "model": "gpt-3.5-turbo-16k",
+        "messages": [
+          {"role": "system", "content": "You are a proficient AI with a specialty in distilling information into key points. Based on the following text, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about."},
+          {"role": "user", "content": `${transcription}`}
+        ]
       },
       {
         headers: {
@@ -561,7 +564,7 @@ const todos= async (req, res) => {
       'https://api.openai.com/v1/chat/completions',
       {
         model: "gpt-3.5-turbo-16k",
-        messages: [{"role": "system", "content": "You are a secretary that deeply reads and understands text transcriptions created in online meetings, You don't miss any points, and create a list of tasks that are suppposed to be done. Each task should be on a new line and should not be numbered."}, {role: "user", content: `${transcription}`}],
+        messages: [{"role": "system","content": "You are an AI expert in analyzing conversations and extracting action items. Please review the text and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done. These could be tasks assigned to specific individuals, or general actions that the group has decided to take. Please list these action items clearly and concisely."}, {role: "user", content: `${transcription}`}],
       },
       {
         headers: {
