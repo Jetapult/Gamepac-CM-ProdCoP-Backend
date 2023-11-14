d94d3c4028728388454ca153fb613b26d3faeb7a
Snehal Shyamsukha <snehalshyamsukha@Snehal-Shyamsukha.local>
2023-11-14 11:18:33 +0530

    updated models

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -448,7 +448,7 @@ const smartActions = async(req,res)=>{
     const openaiResponse = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: "gpt-3.5-turbo-16k",
+        model: "gpt-4-1106-preview",
         messages: [{"role": "system", "content": "You are an expert analyst at a Game Studio, You read all the reviews given to the games from the Play Store or App Store, deeply understand all relevant reviews good and bad. You analyse these reviews and give a recommendation to the founder/developers as to what should be the next steps into improving the game. You list out all the recommendations in crisp points that are consumable and in not more than 200 words."}, {role: "user", content: `${comments}`}],
       },
       {
@@ -477,7 +477,7 @@ const replyAssistant= async (req, res) => {
     const openaiResponse = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: "gpt-3.5-turbo-16k",
+        model: "gpt-4-1106-preview",
         messages: [{"role": "system", "content": "You are a social Media Assistant at a game Studio and you understand the reviews and give appropriate replies to the user."}, {role: "user", content: `${comment}`}],
       },
       {
@@ -505,7 +505,7 @@ const summary= async (req, res) => {
     const openaiResponse = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        "model": "gpt-3.5-turbo-16k",
+        "model": "gpt-4-1106-preview",
         "messages": [
           {"role": "system", "content": "You are a proficient AI with a specialty in distilling information into key points. Based on the following text, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about."},
           {"role": "user", "content": `${transcription}`}
@@ -536,7 +536,7 @@ const title= async (req, res) => {
     const openaiResponse = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: "gpt-3.5-turbo-16k",
+        model: "gpt-4-1106-preview",
         messages: [{"role": "system", "content": "You are a smart Title generator, read the transcript thoroughly and generate a small and crisp title not more than 5 words."}, {role: "user", content: `${transcription}`}],
       },
       {
@@ -563,7 +563,7 @@ const todos= async (req, res) => {
     const openaiResponse = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
-        model: "gpt-3.5-turbo-16k",
+        model: "gpt-4-1106-preview",
         messages: [{"role": "system","content": "You are an AI expert in analyzing conversations and extracting action items. Please review the text and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done. These could be tasks assigned to specific individuals, or general actions that the group has decided to take. Please list these action items clearly and concisely."}, {role: "user", content: `${transcription}`}],
       },
       {
